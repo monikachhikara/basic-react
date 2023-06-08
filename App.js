0428cf476fdd0a4ae3e82a91f26cc33f431c6864
MonikaMalikChhikara <74526919+MonikaMalikChhikara@users.noreply.github.com>
2023-06-07 23:00:32 -0500

    Bit of modification

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
-    const heading = React.createElement("h1", {id:'parent'},[
-    React.createElement('div', {id:"child1"}, [
-        React.createElement('h1',{}, "I am h1 tag"),
-        React.createElement('h2', {}, "I am an h2 tag")
-    ])],
-    [
-        React.createElement('div', {id:"child2"}, [
-            React.createElement('h1',{}, "I am h1 tag"),
-            React.createElement('h2', {}, "I am an h2 tag")
-        ])]);
-    console.log(heading);
+    import React from "react";
+    import ReactDOM  from "react-dom/client";
+    
+    const heading = <h1 className="head">Welcome back</h1>
+
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(heading);
