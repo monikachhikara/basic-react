820c943e574af0d82292f4f4de9c28d3714c72c3
MonikaMalikChhikara <74526919+MonikaMalikChhikara@users.noreply.github.com>
2023-06-13 10:15:07 -0500

    Code seperated in different components

--- App.js
+++
@@ -1,7 +0,0 @@
-    import React from "react";
-    import ReactDOM  from "react-dom/client";
-    
-    const heading = <h1 className="head">Welcome back</h1>
-
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading);
